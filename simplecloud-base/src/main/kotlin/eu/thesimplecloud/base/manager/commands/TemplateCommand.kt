7890ba4fb762bf5659c7f16e4ae052bef4343d04
Frederick <frederick.baier.berlin@outlook.de>
2019-10-10 17:35:08 +0200

    Created template command

---
+++ simplecloud-base/src/main/kotlin/eu/thesimplecloud/base/manager/commands/TemplateCommand.kt
@@ -0,0 +1,45 @@
+package eu.thesimplecloud.base.manager.commands
+
+import eu.thesimplecloud.base.manager.startup.Manager
+import eu.thesimplecloud.launcher.console.command.ICommandHandler
+import eu.thesimplecloud.launcher.console.command.annotations.Command
+import eu.thesimplecloud.launcher.console.command.annotations.CommandArgument
+import eu.thesimplecloud.launcher.console.command.annotations.CommandSubPath
+import eu.thesimplecloud.launcher.startup.Launcher
+import eu.thesimplecloud.lib.CloudLib
+import eu.thesimplecloud.lib.packets.template.PacketIOAddTemplate
+import eu.thesimplecloud.lib.packets.template.PacketIODeleteTemplate
+import eu.thesimplecloud.lib.template.impl.DefaultTemplate
+
+@Command("template", true)
+class TemplateCommand : ICommandHandler {
+
+    val templateManager = CloudLib.instance.getTemplateManager()
+
+    @CommandSubPath("create <name>", "Creates a template")
+    fun createTemplate(@CommandArgument("name") name: String){
+        if (name.length > 16){
+            Launcher.instance.consoleSender.sendMessage("manager.command.template.create.name-too-long", "The specified name must be shorter than 17 characters.")
+        }
+        if (templateManager.getTemplate(name) != null) {
+            Launcher.instance.consoleSender.sendMessage("manager.command.template.create.already-exist", "Template $name does already exist.")
+            return
+        }
+        val template = DefaultTemplate(name)
+        templateManager.addTemplate(template)
+        Manager.instance.nettyServer.getClientManager().sendPacketToAllClients(PacketIOAddTemplate(template))
+        Launcher.instance.consoleSender.sendMessage("manager.command.template.create.success", "Template $name was registered")
+    }
+
+    @CommandSubPath("delete <name>", "")
+    fun deleteTemplate(@CommandArgument("name") name: String){
+        if (templateManager.getTemplate(name) == null) {
+            Launcher.instance.consoleSender.sendMessage("manager.command.template.delete.not-exist", "Template $name does not exist.")
+            return
+        }
+        templateManager.removeTemplate(name)
+        Manager.instance.nettyServer.getClientManager().sendPacketToAllClients(PacketIODeleteTemplate(name))
+        Launcher.instance.consoleSender.sendMessage("manager.command.template.delete.success", "Template $name was deleted.")
+    }
+
+}
