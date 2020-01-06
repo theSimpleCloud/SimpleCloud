d19a405b3266715f00a83bf8e6dd38974f71dc40
Frederick <frederick.baier.berlin@outlook.de>
2020-01-06 18:36:33 +0100

    Fixed property in ShutdownGroupCommand.kt

--- simplecloud-base/src/main/kotlin/eu/thesimplecloud/base/manager/commands/ShutdownGroupCommand.kt
+++ simplecloud-base/src/main/kotlin/eu/thesimplecloud/base/manager/commands/ShutdownGroupCommand.kt
@@ -13,7 +13,7 @@ class ShutdownGroupCommand : ICommandHandler {
     @CommandSubPath("<group>", "Stops all services of a group.")
     fun startService(commandSender: ICommandSender, @CommandArgument("group") cloudServiceGroup: ICloudServiceGroup) {
         cloudServiceGroup.stopAllRunningServices()
-        commandSender.sendMessage("manager.command.stop.success", "Stopping all services of group %GROUP%", cloudServiceGroup.getName())
+        commandSender.sendMessage("manager.command.shutdowngroup.success", "Stopping all services of group %GROUP%", cloudServiceGroup.getName())
     }
 
 }
