d36510a6eed31b3920085d31a2174c523d1e2e4a
Frederick <frederick.baier.berlin@outlook.de>
2020-03-25 19:03:31 +0100

    Created updater project. Improved auto updater.

--- simplecloud-launcher/src/main/kotlin/eu/thesimplecloud/launcher/updater/LauncherUpdater.kt
+++ simplecloud-launcher/src/main/kotlin/eu/thesimplecloud/launcher/updater/LauncherUpdater.kt
@@ -1,17 +1,19 @@
 package eu.thesimplecloud.launcher.updater
 
 
+import eu.thesimplecloud.api.depedency.Dependency
 import eu.thesimplecloud.api.utils.ManifestLoader
 import eu.thesimplecloud.launcher.LauncherMain
 import eu.thesimplecloud.launcher.invoker.MethodInvokeHelper
 import eu.thesimplecloud.launcher.startup.Launcher
 import java.io.File
 import java.net.URLClassLoader
+import kotlin.concurrent.thread
 
 class LauncherUpdater : AbstractUpdater(
         "eu.thesimplecloud.simplecloud",
         "simplecloud-launcher",
-        File(Launcher::class.java.protectionDomain.codeSource.location.toURI())
+        File("launcher-update.jar")
 ) {
 
     override fun getCurrentVersion(): String {
@@ -19,11 +21,20 @@ class LauncherUpdater : AbstractUpdater(
     }
 
     override fun executeJar() {
-        val file = File(Launcher::class.java.protectionDomain.codeSource.location.toURI())
+        val file = File("launcher-update.jar")
+        val runningJar = File(Launcher::class.java.protectionDomain.codeSource.location.toURI())
         val mainClass = ManifestLoader.getMainClass(file.absolutePath)
         val newClassLoader = URLClassLoader(arrayOf(file.toURI().toURL()))
         val mainMethod = newClassLoader.loadClass(mainClass).getMethod("main", Array<String>::class.java)
         Thread.currentThread().contextClassLoader = newClassLoader
+        Runtime.getRuntime().addShutdownHook(Thread {
+            val updaterFile = File("storage/updater.jar")
+            val dependency = Dependency("eu.thesimplecloud.simplecloud", "simplecloud-updater", getLatestVersion()!!)
+            dependency.download(getRepositoryURL(), updaterFile)
+            val processBuilder = ProcessBuilder("java", "-jar", "storage/updater.jar", "1500", runningJar.absolutePath, file.absolutePath)
+            processBuilder.directory(File("."))
+            processBuilder.start()
+        })
         MethodInvokeHelper.invoke(mainMethod, null, LauncherMain.specifiedArguments)
     }
 }
