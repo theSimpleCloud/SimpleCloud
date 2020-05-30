faeac607f61c47ab4bcf51b0bcd063d5ed466a14
Frederick <baier.frederick@gmail.com>
2020-05-30 17:51:53 +0200

    Improved the property system.

--- simplecloud-api/src/main/kotlin/eu/thesimplecloud/api/property/IProperty.kt
+++ simplecloud-api/src/main/kotlin/eu/thesimplecloud/api/property/IProperty.kt
@@ -4,7 +4,14 @@ interface IProperty<T : Any> {
 
     /**
      * Returns the value of this property
+     * @param callerClassLoader the class loader of the caller class. The class loader is used to load the class.
+     * This is required when this function gets called from a module.
      */
-    fun getValue(callerClassLoader: ClassLoader = this::class.java.classLoader): T
+    fun getValue(callerClassLoader: ClassLoader): T
+
+    /**
+     * Returns the value of this property
+     */
+    fun getValue(): T = getValue(this::class.java.classLoader)
 
 }
