ce09a375ca4cd6660e1d09cd1ad75a5a13f661fa
Frederick <baier.frederick@gmail.com>
2020-06-04 12:34:44 +0200

    Add copyright. Add nogui argument for spigot services.

--- simplecloud-modules/simplecloud-module-proxy/src/main/kotlin/eu/thesimplecloud/module/proxy/service/bungee/BungeePluginMain.kt
+++ simplecloud-modules/simplecloud-module-proxy/src/main/kotlin/eu/thesimplecloud/module/proxy/service/bungee/BungeePluginMain.kt
@@ -1,18 +1,32 @@
+/*
+ * MIT License
+ *
+ * Copyright (C) 2020 SimpleCloud-Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
 package eu.thesimplecloud.module.proxy.service.bungee
 
-import eu.thesimplecloud.api.CloudAPI
 import eu.thesimplecloud.api.player.text.CloudText
-import eu.thesimplecloud.api.sync.`object`.SynchronizedObjectHolder
-import eu.thesimplecloud.module.proxy.config.Config
-import eu.thesimplecloud.module.proxy.config.DefaultConfig
-import eu.thesimplecloud.module.proxy.config.ProxyGroupConfiguration
 import eu.thesimplecloud.module.proxy.config.TablistConfiguration
-import eu.thesimplecloud.module.proxy.extensions.mapToLowerCase
 import eu.thesimplecloud.module.proxy.service.ProxyHandler
 import eu.thesimplecloud.module.proxy.service.bungee.listener.BungeeListener
 import eu.thesimplecloud.plugin.proxy.bungee.text.CloudTextBuilder
-import eu.thesimplecloud.plugin.startup.CloudPlugin
-import net.md_5.bungee.api.ChatColor
 import net.md_5.bungee.api.connection.ProxiedPlayer
 import net.md_5.bungee.api.plugin.Plugin
 import java.util.concurrent.TimeUnit
