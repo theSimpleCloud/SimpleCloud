82a36ae02afa94dc05f9b256190316e4b509e350
Frederick <baier.frederick@gmail.com>
2020-06-18 17:14:51 +0200

    Remove unnecessary update

--- simplecloud-plugin/src/main/kotlin/eu/thesimplecloud/plugin/proxy/ProxyEventHandler.kt
+++ simplecloud-plugin/src/main/kotlin/eu/thesimplecloud/plugin/proxy/ProxyEventHandler.kt
@@ -133,9 +133,7 @@ object ProxyEventHandler {
             cloudPlayer.let { CloudAPI.instance.getCloudPlayerManager().delete(it) }
             //send update that the player is now offline
             val client = CloudPlugin.instance.communicationClient
-            CloudAPI.instance.getCloudPlayerManager().sendUpdateToConnection(cloudPlayer, client).addCompleteListener {
-                CloudAPI.instance.getCloudPlayerManager().sendDeleteToConnection(cloudPlayer, client)
-            }
+            CloudAPI.instance.getCloudPlayerManager().sendDeleteToConnection(cloudPlayer, client)
         }
 
         subtractOneFromOnlineCount(CloudPlugin.instance.thisService())
