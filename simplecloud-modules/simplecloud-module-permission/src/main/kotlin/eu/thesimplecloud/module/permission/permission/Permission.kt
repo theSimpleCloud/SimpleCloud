5c08b5090028d19f2993196f1c4a31f0110faaae
Frederick <baier.frederick@gmail.com>
2020-06-24 17:41:58 +0200

    Add * permissions to the permission module

--- simplecloud-modules/simplecloud-module-permission/src/main/kotlin/eu/thesimplecloud/module/permission/permission/Permission.kt
+++ simplecloud-modules/simplecloud-module-permission/src/main/kotlin/eu/thesimplecloud/module/permission/permission/Permission.kt
@@ -27,4 +27,44 @@ data class Permission(val permissionString: String, val timeoutTimestamp: Long, 
 
     fun isExpired(): Boolean = (System.currentTimeMillis() > timeoutTimestamp) && timeoutTimestamp != -1L
 
+
+    fun matches(permission: String): Boolean {
+        return if (isExpired() || !active) {
+            false
+        } else {
+            return matchesPermission(permission)
+        }
+    }
+
+    private fun matchesPermission(permission: String): Boolean {
+        val endsWithStar = this.permissionString.endsWith(".*")
+        return if (endsWithStar) {
+            matchesWithStarAtTheEnd(permission)
+        } else {
+            isPermissionToCheckEqualToThisPermission(permission)
+        }
+    }
+
+    private fun isPermissionToCheckEqualToThisPermission(permission: String): Boolean {
+        return permission == this.permissionString
+    }
+
+    private fun matchesWithStarAtTheEnd(permission: String): Boolean {
+        val checkPermissionComponents = permission.split(".")
+        val thisPermissionComponents = this.permissionString.split(".")
+
+        val isPermissionToCheckLongEnough = checkPermissionComponents.size >= thisPermissionComponents.size
+        return if (isPermissionToCheckLongEnough) {
+            areAllPermissionComponentsEqual(checkPermissionComponents, thisPermissionComponents)
+        } else {
+            false
+        }
+    }
+
+    private fun areAllPermissionComponentsEqual(checkPermissionComponents: List<String>, thisPermissionComponents: List<String>): Boolean {
+        val thisPermissionComponentsWithoutStar = thisPermissionComponents.dropLast(1)
+        val checkComponentsWithLimit = checkPermissionComponents.take(thisPermissionComponentsWithoutStar.size)
+        return checkComponentsWithLimit == thisPermissionComponentsWithoutStar
+    }
+
 }
