d665bf58417b1f2731e1175b66f84717e6926a67
Frederick <baier.frederick@gmail.com>
2020-11-05 14:27:25 +0100

    SimpleCloud-Plugin: Fix hover event and appended CloudText were not shown on Velocity

--- simplecloud-plugin/src/main/kotlin/eu/thesimplecloud/plugin/proxy/velocity/text/CloudTextBuilder.kt
+++ simplecloud-plugin/src/main/kotlin/eu/thesimplecloud/plugin/proxy/velocity/text/CloudTextBuilder.kt
@@ -70,7 +70,8 @@ class CloudTextBuilder {
             i++
         }
         text = stringBuilder.toString()
-        val component = Component.text(text)
+        val component = Component.text()
+        component.content(text)
 
         val hover = cloudText.hover
         if (hover != null) {
@@ -87,7 +88,7 @@ class CloudTextBuilder {
             component.append(componentToAppend)
         }
 
-        return component
+        return component.build()
     }
 
 }
