147d19cdd85684087595e42fb19037cdf6ed0c77
D151l <d151l@d151l.de>
2022-06-07 14:04:45 +0200

    add ServiceStateCommandSuggestionProvider

---
+++ simplecloud-launcher/src/main/kotlin/eu/thesimplecloud/launcher/console/command/provider/ServiceStateCommandSuggestionProvider.kt
@@ -0,0 +1,13 @@
+package eu.thesimplecloud.launcher.console.command.provider
+
+import eu.thesimplecloud.api.CloudAPI
+import eu.thesimplecloud.api.command.ICommandSender
+import eu.thesimplecloud.api.service.ServiceState
+
+class ServiceStateCommandSuggestionProvider : ICommandSuggestionProvider {
+
+    override fun getSuggestions(sender: ICommandSender, fullCommand: String, lastArgument: String): List<String> {
+        return ServiceState.values().map { it.name.lowercase() }
+    }
+
+}
