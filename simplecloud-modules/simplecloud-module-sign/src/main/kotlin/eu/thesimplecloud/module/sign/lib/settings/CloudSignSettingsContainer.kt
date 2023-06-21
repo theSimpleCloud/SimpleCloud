672c53d16e5805acedcce85ab31d66a6c16bd727
MrManHD <57962465+MrManHD@users.noreply.github.com>
2023-06-21 12:45:02 +0200

    add sign delay settings

---
+++ simplecloud-modules/simplecloud-module-sign/src/main/kotlin/eu/thesimplecloud/module/sign/lib/settings/CloudSignSettingsContainer.kt
@@ -0,0 +1,10 @@
+package eu.thesimplecloud.module.sign.lib.settings
+
+/**
+ * Created by MrManHD
+ * Class create at 21.06.2023 12:12
+ */
+
+class CloudSignSettingsContainer(
+    val updateSignDelay: Long
+)
