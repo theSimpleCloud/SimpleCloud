235c2cf014364254aa99e09d610e3655ad9da934
Jannik <jannik.developer02@gmail.com>
2023-08-18 23:55:05 +0200

    [/] now checking if plugin exists via bukkit instead of Class.forName, try catch, ...

--- simplecloud-modules/simplecloud-module-placeholders/src/main/kotlin/eu/thesimplecloud/module/placeholders/spigot/BukkitPluginMain.kt
+++ simplecloud-modules/simplecloud-module-placeholders/src/main/kotlin/eu/thesimplecloud/module/placeholders/spigot/BukkitPluginMain.kt
@@ -1,46 +1,28 @@
 package eu.thesimplecloud.module.placeholders.spigot
 
 import eu.thesimplecloud.module.placeholders.spigot.placeholderapi.PlaceholderHook
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class BukkitPluginMain : JavaPlugin() {
 
-    var replacePermissionModule = false
-    var replaceChatTabModule = false
+    val replacePermissionModule = Bukkit.getPluginManager().getPlugin("SimpleCloud-Permission") != null
+    val replaceChatTabModule = Bukkit.getPluginManager().getPlugin("SimpleCloud-Chat-Tab") != null
 
     override fun onEnable() {
 
-        checkPermissionsModule()
-        checkChatTabModule()
+        if(!replacePermissionModule) {
+            println("Could not replace placeholders for permission module. Please make sure that the permission module is loaded correctly if you want to use placeholders for that.")
+        }
+
+        if(!replaceChatTabModule) {
+            println("Could not replace placeholders for chat-tab module. Please make sure that the chat-tab module is loaded correctly if you want to use placeholders for that.")
+        }
 
         val register = PlaceholderHook(this).register()
 
         if(register) println("[INFO] Registered PlaceholderAPI hook successfully!")
         else println("[FATAL] Failed to register PlaceholderAPI hook!")
-    }
-
-    fun checkPermissionsModule() {
-        try {
-            val forName = Class.forName("eu.thesimplecloud.module.permission.manager.PermissionModule")
-            println("${forName.name} exists, will replace placeholders for permission module.")
-            replacePermissionModule = true
-
-        } catch (exception: Exception) {
-            println("Could not replace placeholders for permission module. Please make sure that the permission module is loaded correctly if you want to use placeholders for that.")
-            exception.printStackTrace()
-        }
-    }
-
-    fun checkChatTabModule() {
-        try {
-            val forName = Class.forName("eu.thesimplecloud.module.prefix.manager.PrefixModule")
-            println("${forName.name} exists, will replace placeholders for chat-tab module.")
-            replaceChatTabModule = true
-
-        } catch (exception: Exception) {
-            println("Could not replace placeholders for chat-tab module. Please make sure that the chat-tab module is loaded correctly if you want to use placeholders for that.")
-            exception.printStackTrace()
-        }
     }
 
 }
