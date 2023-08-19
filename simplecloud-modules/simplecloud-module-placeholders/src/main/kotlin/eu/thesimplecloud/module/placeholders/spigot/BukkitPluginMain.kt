3fbc7e255a15cda89d26cabca02cf41545abf79a
Jannik <63158842+rlqu@users.noreply.github.com>
2023-08-19 10:59:14 +0200

    Update simplecloud-modules/simplecloud-module-placeholders/src/main/kotlin/eu/thesimplecloud/module/placeholders/spigot/BukkitPluginMain.kt
    Initialization now works with by lazy
    
    Co-authored-by: Fllip <39161142+FllipEis@users.noreply.github.com>

--- simplecloud-modules/simplecloud-module-placeholders/src/main/kotlin/eu/thesimplecloud/module/placeholders/spigot/BukkitPluginMain.kt
+++ simplecloud-modules/simplecloud-module-placeholders/src/main/kotlin/eu/thesimplecloud/module/placeholders/spigot/BukkitPluginMain.kt
@@ -7,7 +7,7 @@ import org.bukkit.plugin.java.JavaPlugin
 class BukkitPluginMain : JavaPlugin() {
 
     val replacePermissionModule = Bukkit.getPluginManager().getPlugin("SimpleCloud-Permission") != null
-    val replaceChatTabModule = Bukkit.getPluginManager().getPlugin("SimpleCloud-Chat-Tab") != null
+    val replaceChatTabModule by lazy { Bukkit.getPluginManager().getPlugin("SimpleCloud-Chat-Tab") != null }
 
     override fun onEnable() {
 
