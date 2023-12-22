3d9fd34614abc0deb4ac65ac6bc0e60d3f0f74f7
Dominic <d151l@d151l.de>
2023-12-22 15:18:16 +0100

    add converter for the Xms support

---
+++ simplecloud-base/src/main/kotlin/eu/thesimplecloud/base/manager/update/converter/Converter_2_6_To_2_7.kt
@@ -0,0 +1,35 @@
+package eu.thesimplecloud.base.manager.update.converter
+
+import eu.thesimplecloud.api.directorypaths.DirectoryPaths
+import eu.thesimplecloud.jsonlib.JsonLib
+import java.io.File
+
+class Converter_2_6_To_2_7: IVersionConverter {
+
+    override fun getTargetMinorVersion(): Int {
+        return 7
+    }
+
+    override fun convert() {
+        convertGroupsInDirectory(File(DirectoryPaths.paths.proxyGroupsPath))
+        convertGroupsInDirectory(File(DirectoryPaths.paths.lobbyGroupsPath))
+        convertGroupsInDirectory(File(DirectoryPaths.paths.serverGroupsPath))
+    }
+
+    private fun convertGroupsInDirectory(dir: File) {
+        dir.listFiles()?.forEach {
+            convertSingleGroupByFile(it)
+        }
+    }
+
+    private fun convertSingleGroupByFile(file: File) {
+        val groupJsonLib = JsonLib.fromJsonFile(file) ?: return
+        if (groupJsonLib.jsonElement.asJsonObject.has("minimumMemory")) {
+            return
+        }
+        val memory: Int = groupJsonLib.getInt("memory") ?: 0
+
+        groupJsonLib.append("minimumMemory", memory / 2)
+        groupJsonLib.saveAsFile(file)
+    }
+}
